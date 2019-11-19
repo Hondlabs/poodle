0f3490a27b074bb7fdc7f38488a8f986320693c7
mertcankirtay <kirtay.mertcan@gmail.com>
2019-11-19 22:06:28 +0300

    workspace component implemented and ready for prototyping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShellComponent } from './shell/shell.component';
 import { SplashComponent } from './splash/splash.component';
+import { WorkspaceComponent } from './shell/workspace/workspace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShellComponent,
-    SplashComponent
+    SplashComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule
