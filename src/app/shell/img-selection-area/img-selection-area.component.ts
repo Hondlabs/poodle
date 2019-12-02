f7775526f3cf06865c6d2bf567548a3c13509a9e
mertcankirtay <kirtay.mertcan@gmail.com>
2019-12-02 19:56:10 +0300

    image selection area image selection task implemented

--- src/app/shell/img-selection-area/img-selection-area.component.ts
+++ src/app/shell/img-selection-area/img-selection-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/shared/services/global.service';
 
 @Component({
   selector: 'app-img-selection-area',
@@ -9,7 +10,7 @@ export class ImgSelectionAreaComponent implements OnInit {
 
   imageURLs = [];
 
-  constructor() {
+  constructor(public globalService: GlobalService) {
 
     for (let i = 0; i < 77; i++) {
 
