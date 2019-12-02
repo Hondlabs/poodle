e2e74025f39b9e336181b18cc8f6e167cb269d5e
mertcankirtay <kirtay.mertcan@gmail.com>
2019-12-02 15:00:44 +0300

    template selection area implemented

--- src/app/shell/template-selection-area/template-selection-area.component.ts
+++ src/app/shell/template-selection-area/template-selection-area.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/shared/services/global.service';
+import { WorkspaceComponent } from '../workspace/workspace.component';
 
 @Component({
   selector: 'app-template-selection-area',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateSelectionAreaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public globalService: GlobalService, ) { }
 
   ngOnInit() {
+  }
+
+  setTemplateNumber(number: string) {
+
+    this.globalService.setTemplateNumber(parseInt(number));
+    new WorkspaceComponent(this.globalService);
   }
 
 }
